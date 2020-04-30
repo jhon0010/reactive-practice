da931f9ebaf2d25d330508ce138d0e5c154d157f
jhon0010 <jhoning.soft@gmail.com>
2020-04-30 11:34:59 -0500

    Reactive reference practice

---
+++ src/main/java/com/practice/reactor/core/transformations/IndexFlux.java
@@ -0,0 +1,26 @@
+package com.practice.reactor.core.transformations;
+
+import org.slf4j.Logger;
+import reactor.core.publisher.Flux;
+
+import java.util.List;
+
+import static java.util.Arrays.asList;
+import static org.slf4j.LoggerFactory.getLogger;
+
+public class IndexFlux {
+
+    private static final Logger LOGGER = getLogger(IndexFlux.class);
+
+    public static void main(String[] args) {
+        List<String> names = asList("John", "Doe", "Bean", "Peter");
+
+        String someQuestion = "";
+
+        Flux.fromIterable(names)
+                .index()
+                .doOnNext(tuple -> LOGGER.info("List with index {} and value {}", tuple.getT1(), tuple.getT2()))
+                .subscribe();
+    }
+
+}
